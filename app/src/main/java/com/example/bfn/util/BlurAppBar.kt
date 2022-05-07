bdf892afc096550f66db9753a51d40c1c32f6ca2
el.Koutheir <Koutheir.elbehi@digitarm.com>
2022-05-07 17:05:36 +0100

    add book details

---
+++ app/src/main/java/com/example/bfn/util/BlurAppBar.kt
@@ -0,0 +1,37 @@
+package com.example.bfn.util
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.appbar.AppBarLayout
+import jp.wasabeef.blurry.Blurry
+
+class BlurAppBar(val context: Context) {
+
+
+    var blurred = false
+    fun blurAppBar(appBar: AppBarLayout, blurro: ConstraintLayout) {
+        Blurry.with(context)
+            .radius(6)
+            .sampling(2)
+            .async()
+            .onto(blurro as ViewGroup)
+        appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { p0, p1 ->
+            val alpha = (p0.totalScrollRange + p1).toFloat() / p0.totalScrollRange
+            if ((alpha == 0f || alpha == 1f)) {
+                Blurry.delete(blurro as ViewGroup)
+                blurred = false
+            } else if ((alpha > 0 && alpha < 1) && !blurred) {
+                blurred = true
+                Blurry.with(context)
+                    .radius(25)
+                    .sampling(2)
+                    .async()
+                    .animate(125)
+                    .onto(blurro as ViewGroup)
+
+            }
+
+        })
+    }
+}
